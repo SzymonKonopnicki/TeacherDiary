df85e6413ecb8050e4f2dc5ccf57ecc329188a6f
Czarno Mysz <szymonkonopnicki@outlook.com>
2024-02-08 21:08:00 +0100

    Dodaj pliki projektów.

---
+++ TeacherDiary.WebApi/Database/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace TeacherDiary.WebApi.Database.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Agreement { get; set; }
+        public string Comments { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
---
+++ TeacherDiary.WebApi/Database/Entities/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace TeacherDiary.WebApi.Database.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public bool Active { get; set; }
+        public int EntryQuantity { get; set; }
+        public int AvailableEntryQuantity { get; set; }
+    }
+}
